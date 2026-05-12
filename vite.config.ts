ef83f2de472c9c117df7c56bc52da67fe811ab69
Ade Surya Ananda <adsurkasur.dev@gmail.com>
2026-05-12 21:10:47 +0700

    feat: initialize TanStack Router route tree and update Vite configuration plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({
-      routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/routeTree.gen.ts",
-    }),
     tsconfigPaths(),
     tailwindcss(),
     tanstackStart({
       appRoot: "./src",
     }),
+    react(),
   ],
 });
